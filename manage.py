d9c2aae3806252f45c81103bea7bcaa2ef686176
sguthery <skittely@gmail.com>
2017-10-25 16:58:12 -0500

    Added manage script for testing

---
+++ manage.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+"""
+Manage app
+"""
+from flask_script import Manager
+from backend import app
+import unittest
+
+manager = Manager(app)
+
+
+@manager.command
+def test():
+    """Run unit tests"""
+    tests = unittest.TestLoader().discover('backend/tests', pattern='test*.py')
+    print(tests)
+    result = unittest.TextTestRunner().run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
+
+
+if __name__ == '__main__':
+    manager.run()
